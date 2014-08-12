a9ecb84f2c351a885d7136a2b5a9f26efd59b5bb
Leon de Rijke <leon@cmsworks.nl>
2014-08-12 06:20:27 +0200

    Add Browser Sync config

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var browserSync = require('browser-sync');
 var gulp = require('gulp');
 var header = require('gulp-header');
 var minifyCSS = require('gulp-minify-css');
@@ -34,10 +35,24 @@ gulp.task('build-css', function() {
 	return gulp.src('src/css/modern-blink.css')
 		.pipe(header(extended, { pkg : pkg } ))
 		.pipe(gulp.dest('./dist'))
+		.pipe(browserSync.reload({stream: true}))
 		.pipe(rename('modern-blink.min.css'))
 		.pipe(minifyCSS({keepSpecialComments: 0}))
 		.pipe(header(succint, { pkg: pkg } ))
 		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: './'
+		}
+	});
+});
+
+gulp.task('watch', ['build-js', 'build-css', 'browser-sync'], function() {
+	gulp.watch('src/js/*.js', ['build-js']);
+	gulp.watch('src/css/*.css', ['build-css']);
 });
 
 gulp.task('default', ['build-js', 'build-css']);
